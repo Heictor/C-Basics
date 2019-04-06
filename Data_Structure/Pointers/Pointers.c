f436198c001150ea310bb2f728234e4be7b13db8
Heictor <heictor8@gmail.com>
2019-04-06 19:10:18 -0300

    Commit #3

---
+++ Data_Structure/Pointers/Pointers.c
@@ -0,0 +1,20 @@
+#include <stdio.h>/* code */
+
+// "*" Used on pointers to return the variable's content
+// "&" Used on pointers to return the variable's memory adress
+int main(int argc, char const *argv[]) {
+    int *pNumber;
+    int Number = 5;
+    pNumber = &Number;
+    printf("%d\n",Number);
+    printf("%d\n",*pNumber);
+    printf("%p\n",&pNumber); //%p to print adress
+
+    struct pStruct{
+        int i;
+        float f;
+    }typedef pStruct;
+
+    pStruct *pStruct;
+    return 0;
+}
