6100e9ac07d0458e9d944f0ac03b62f28f0e5f88
Heictor <heictorcosta@yahoo.com.br>
2019-04-20 11:11:03 -0300

    Commit #6

--- Data_Structure/Memory_Allocation/malloc_&_sizeof_Example.c
+++ Data_Structure/Memory_Allocation/malloc_&_sizeof_Example.c
@@ -1,5 +1,5 @@
 #include <stdio.h>
-#include <stdlib.h>
+#include <malloc.h>
 #define maxHeight 200
 
 struct WHght{
@@ -8,12 +8,13 @@ struct WHght{
 }typedef WHght;
 
 int main(){
-    WHght firstPerson;
-    firstPerson.weight = 72;
-    firstPerson.height = 176;
+    WHght* firstPerson = (WHght*) malloc(sizeof(WHght));;
+    firstPerson->weight = 72;
+    firstPerson->height = 176;
+
     printf("weight: %i, height: %i.\n",
-    firstPerson.weight,firstPerson.height);
-    if(firstPerson.height>maxHeight){
+    firstPerson->weight,firstPerson->height);
+    if(firstPerson->height>maxHeight){
         printf("Height above max limit.\n");
     }else{
         printf("Height minor the max limit.\n");
--- Data_Structure/Pointers/Pointers.c
+++ Data_Structure/Pointers/Pointers.c
@@ -2,19 +2,11 @@
 
 // "*" Used on pointers to return the variable's content
 // "&" Used on pointers to return the variable's memory adress
-int main(int argc, char const *argv[]) {
-    int *pNumber;
-    int Number = 5;
-    pNumber = &Number;
-    printf("%d\n",Number);
-    printf("%d\n",*pNumber);
-    printf("%p\n",&pNumber); //%p to print adress
-
-    struct pStruct{
-        int i;
-        float f;
-    }typedef pStruct;
-
-    pStruct *pStruct;
+int main() {
+    int x = 30;
+    printf("Value of x: %i\n", x);
+    int* y = &x; //y will be a pointer of x's memory adress
+    *y = 50; //The content which y is pointing will now be replaced by 50
+    printf("New value of x: %i\n", x);
     return 0;
 }
